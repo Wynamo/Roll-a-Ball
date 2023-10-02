3863b57ade179d6f6c4ffd7d537eb051eb62096d
Charlie <charlieryancurry@gmail.com>
2023-10-02 16:08:34 -0300

    add prototype text box

---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public TextMeshProUGUI theText;
+
+    public int currentLine;
+    public int endAtLine;
+
+    public PlayerController player;
+
+    public TextAsset textFile;
+    public string[] textLines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+
+    }
+
+    private void Update()
+    {
+        theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return)) 
+        {
+            currentLine += 1;
+        }
+    }
+}
