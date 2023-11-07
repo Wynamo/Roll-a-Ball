648fc8b342de89d3adb88ae388495567fcda8656
Charlie <charlieryancurry@gmail.com>
2023-11-07 11:30:25 -0400

    add heart system

--- Assets/Scripts/Lives.cs
+++ Assets/Scripts/Lives.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class Lives : MonoBehaviour
 {
+
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    public Sprite fullHeart;
+    public Sprite threeQuarterHeart;
+    public Sprite halfHeart;
+    public Sprite quarterHeart;
+    public Sprite emptyHeart;
 
     private int currentLives;
     //public int startingLives = 3;
@@ -18,7 +31,6 @@ public class Lives : MonoBehaviour
     public int currentHealth;
 
     public GameObject quitButton;
-    public TextMeshProUGUI healthText;
     public GameObject gameOverTextObject;
     public TextMeshProUGUI livesText;
 
@@ -31,7 +43,7 @@ public class Lives : MonoBehaviour
         gameOverTextObject.SetActive(false);
         currentLives = PlayerPrefs.GetInt("lives");
         SetLivesText();
-        SetHealthText();
+        InitHearts();
         initialPosition = transform.position;
         restartButton.SetActive(false);
     }
@@ -41,21 +53,36 @@ public class Lives : MonoBehaviour
         currentLives--;
         currentHealth = maxHealth;
         transform.position = initialPosition;
-        SetHealthText();
+        InitHearts();
         SetLivesText();
+    }
+
+    private void InitHearts()
+    {
+        heart1.GetComponent<Image>().sprite = fullHeart;
+        heart2.GetComponent<Image>().sprite = fullHeart;
+        heart3.GetComponent<Image>().sprite = fullHeart;
     }
 
     void OnHit()
     {
         currentHealth--;
 
-        if (currentHealth <= 0)
+        switch (currentHealth)
         {
-            OnDeath();
-        }
-        else
-        {
-            SetHealthText();
+            case 0:  heart3.GetComponent<Image>().sprite = emptyHeart; OnDeath(); break;
+            case 1:  heart3.GetComponent<Image>().sprite = quarterHeart; break;
+            case 2:  heart3.GetComponent<Image>().sprite = halfHeart; break;
+            case 3:  heart3.GetComponent<Image>().sprite = threeQuarterHeart; break;
+            case 4:  heart2.GetComponent<Image>().sprite = emptyHeart; break;
+            case 5:  heart2.GetComponent<Image>().sprite = quarterHeart; break;
+            case 6:  heart2.GetComponent<Image>().sprite = halfHeart; break;
+            case 7:  heart2.GetComponent<Image>().sprite = threeQuarterHeart; break;
+            case 8:  heart1.GetComponent<Image>().sprite = emptyHeart; break;
+            case 9:  heart1.GetComponent<Image>().sprite = quarterHeart; break;
+            case 10: heart1.GetComponent<Image>().sprite = halfHeart; break;
+            case 11: heart1.GetComponent<Image>().sprite = threeQuarterHeart; break;
+            case 12: break;
         }
     }
 
@@ -76,11 +103,6 @@ public class Lives : MonoBehaviour
             currentLives = PlayerPrefs.GetInt("lives");
             Time.timeScale = 0f;
         }
-    }
-
-    private void SetHealthText()
-    {
-        healthText.text = "Health: " + currentHealth.ToString();
     }
 
     private void OnCollisionEnter(Collision collision)
